9ad4e69f36c8a7c8068a746c0e2612a8d924d314
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-28 18:55:54 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -12,7 +12,9 @@ def assembled_aoh
   assembled_aoh = [
     { name:  "Don Gately", occupation: "Live-in Staff" }, 
     { name:  "Joelle van Dyne", occupation: "Radio Personality" }, 
-    
+    { name:  "Pat Monteseian", occupation: "Staff" }, 
+    { name:  "Kate Gompert", occupation: "None" },
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
 end
 
